8082aafdf2807daaf10f60d3ac8bc9ec5b618bdc
Austin Mullen <mullen13@purdue.edu>
2017-03-03 20:39:19 -0500

    Update 1.0

---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+    public TileType[] tilesTypes;
+    int[,] tiles;
+
+    int mapSizeX = 10;
+    int mapSizeY = 10;
+
+    void Start()
+    {
+        // allocate map tiles
+        tiles = new int[mapSizeX, mapSizeY];
+        // initialize map tiles
+        for(int x = 0; x < mapSizeX; x++)
+        {
+            for(int y = 0; y < mapSizeY; y++)
+            {
+                tiles[x, y] = 0;
+            }
+        }
+    }
+}
