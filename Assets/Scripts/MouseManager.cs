4dfa8f1daf9b92e85f342fa268bf601455268d05
Weakling <mullen13@purdue.edu>
2017-03-09 23:54:58 -0500

    5.0

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -9,6 +9,8 @@ public class MouseManager : MonoBehaviour {
     public bool mapMaking;
     public MapMaker mapMaker;
 
+    public Unit selectedUnit;
+
 	void Start ()
     {
         mapMaker = FindObjectOfType<MapMaker>().GetComponent<MapMaker>();	
@@ -38,24 +40,47 @@ public class MouseManager : MonoBehaviour {
                 // over a hex..
                 if (ourHitObject.transform.parent.GetComponent<Hex>() != null)
                 {
-                    MouseOverHex(ourHitObject);
+                    Debug.Log("hex");
+                    MouseClickHex(ourHitObject);
+                }
+                // over a unit..
+                else if(ourHitObject.transform.parent.GetComponent<Unit>() != null)
+                {
+                    Debug.Log(ourHitObject.name);
+                    MouseOverUnit(ourHitObject);
                 }
             }
-
         }
 
     }
 
-    void MouseOverHex(GameObject ourHitObject)
+    void MouseClickHex(GameObject ourHitObject)
     {
 
         //MeshRenderer mr = ourHitObject.GetComponentInChildren<MeshRenderer>();
-        ourHitObject.GetComponent<MeshRenderer>().materials[0].color = Color.red;
-        ourHitObject.transform.parent.GetComponent<Hex>().GetNeighbours();
+
+        //ourHitObject.GetComponent<MeshRenderer>().materials[0].color = Color.red;
+        //ourHitObject.transform.parent.GetComponent<Hex>().GetNeighbours();
+        if(ourHitObject.name == "default")
+        {
+            GameObject gO = (GameObject)Instantiate(mapMaker.tilesTypes[0].tileVisualPrefab, ourHitObject.transform.position, Quaternion.identity);
+            gO.GetComponentInChildren<MeshRenderer>().materials[0].color = Color.green;
+            Destroy(ourHitObject);
+        }
+
+        if(selectedUnit != null)
+        {
+            selectedUnit.destination = ourHitObject.transform.position;
+        }
 
         //mr.materials[0].color = Color.red;
 
         //Ray fpsRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+    }
 
+    void MouseOverUnit(GameObject ourHitObject)
+    {
+        selectedUnit = ourHitObject.transform.parent.GetComponent<Unit>();
+        Debug.Log(selectedUnit);
     }
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,6 +7,23 @@ public class Unit : MonoBehaviour {
     public int tileX;
     public int tileY;
 
+    public Vector3 destination;
+    float speed = 2;
+
+    void Start()
+    {
+        destination = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 dir = destination - transform.position;
+        Vector3 velocity = dir.normalized * speed * Time.deltaTime;
+
+        // make sure velocity doens't exeed the distance we want
+        velocity = Vector3.ClampMagnitude(velocity, dir.magnitude);
+        transform.Translate(velocity);
+    }
     public void MoveToTile(int x, int y)
     {
 
