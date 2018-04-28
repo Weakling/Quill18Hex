2a55b5c19edbfaba34ed81b964914087d07b4de1
Weakling <mullen13@purdue.edu>
2018-04-27 22:51:07 -0400

    21.0
    Fixed some minor lighting bugs. Made a main menu and map test scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene(2);
+        }
+	}
+
+
+
+    public void GOMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GOTestGameplay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GOMapMaker()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
